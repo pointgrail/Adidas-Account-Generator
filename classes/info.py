52156e1fb78adbcce311b6055f6f977eb56a7e9f
zruss11 <zach@zruss.org>
2017-06-08 18:22:06 -0400

    Create info.py
    updated

--- classes/info.py
+++ classes/info.py
@@ -2,28 +2,29 @@ from US import US
 from UK import UK
 from AU import AU
 from CA import CA
-def info():
 
-	print("Account Generator Ready")
-	location = raw_input('Enter Location US UK CA AU \t')
-	if location == 'US':
-		x = raw_input('My guy how many accounts are we making?')
-		x = int(x)
-		domain = raw_input('My guy which domain do you need?')
-		US(x, domain)
-	if location == 'UK':
-		x = raw_input('My guy how many accounts are we making?')
-		x = int(x)
-		domain = raw_input('My guy which domain do you need?')
-		UK(x, domain)
-	if location == 'AU':
-		x = raw_input('My guy how many accounts are we making?')
-		x = int(x)
-		domain = raw_input('My guy which domain do you need?')
-		AU(x, domain)
-	if location == 'CA':
-		x = raw_input('My guy how many accounts are we making?')
-		x = int(x)
-		domain = raw_input('My guy which domain do you need?')
-		CA(x, domain)
-	
+class INFO:
+	def info(self):
+		print("Account Generator Ready")
+		location = raw_input('Enter Location US UK CA AU \t')
+		if location == 'US':
+			x = raw_input('Number of accounts to be made:')
+			x = int(x)
+			domain = raw_input('Enter in your domain:')
+			US(x, domain)
+		if location == 'UK':
+			x = raw_input('Number of accounts to be made:')
+			x = int(x)
+			domain = raw_input('Enter in your domain:')
+			UK(x, domain)
+		if location == 'AU':
+			x = raw_input('Number of accounts to be made:')
+			x = int(x)
+			domain = raw_input('Enter in your domain:')
+			AU(x, domain)
+		if location == 'CA':
+			x = raw_input('Number of accounts to be made:')
+			x = int(x)
+			domain = raw_input('Enter in your domain:')
+			CA(x, domain)
+			
