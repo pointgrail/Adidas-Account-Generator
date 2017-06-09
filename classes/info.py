6a99368caa4b1a0ba20183471b04011bf6299db5
Simmy <simmy@simmybot.com>
2017-06-09 13:40:53 -0400

    AU redone.

--- classes/info.py
+++ classes/info.py
@@ -1,10 +1,11 @@
 from US import USGEN 
 from UK import UKGEN
-from AU import AU
+from AU import AUGEN 
 from CA import CA
 
 USGEN = USGEN()
 UKGEN = UKGEN()
+AUGEN = AUGEN()
 
 
 class Info:
@@ -20,7 +21,7 @@ class Info:
 			USGEN.US(x, domain)
 		
 		if location == 'UK':
-			UK(x, domain)
+			UKGEN.UK(x, domain)
 		
 		if location == 'AU':
 			AU(x, domain)
